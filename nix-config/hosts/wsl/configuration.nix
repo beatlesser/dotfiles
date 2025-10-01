990bd202527dc27ee6797ed360f440c453cead16
cyer <cyer@wsl.localdomain>
2025-10-01 21:48:28 +0800

    update configs

--- nix-config/hosts/wsl/configuration.nix
+++ nix-config/hosts/wsl/configuration.nix
@@ -3,35 +3,6 @@
   wsl.enable = true;
   wsl.defaultUser = "${username}";
 
-  environment.systemPackages = (
-    with pkgs; [
-      # System Packages
-      curl
-      wget
-      duf # 查看系统磁盘的空间使用情况 better df
-      eza # better ls
-      killall # better kill
-      openssl # SSL/TLS 安全通信、证书管理和加密。
-      jq # 处理 JSON 数据
-      unzip
-      fzf
-      bat # better cat
-      fd # better find
-      zoxide
-      duf # better du
-      ripgrep # better grep
-      file
-      dos2unix
-      just
-      git
-
-      cliphist # 管理和查看剪贴板历史记录
-      wl-clipboard # 命令行工具，操作剪贴板
-
-      fastfetch
-      btop
-    ]
-  );
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
---
+++ nix-config/hosts/wsl/packages.nix
@@ -0,0 +1,39 @@
+{stable,...}: {
+  programs = {
+    fish.enable = true;
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
+    };
+  };
+
+  environment.systemPackages = (
+    with stable; [
+      # System Packages
+      curl
+      wget
+      duf # 查看系统磁盘的空间使用情况 better df
+      lsd # better ls
+      killall # better kill
+      openssl # SSL/TLS 安全通信、证书管理和加密。
+      jq # 处理 JSON 数据
+      unzip
+      fzf
+      bat # better cat
+      fd # better find
+      duf # better du
+      ripgrep # better grep
+      file
+      dos2unix
+      just
+      git
+
+      cliphist # 管理和查看剪贴板历史记录
+      wl-clipboard # 命令行工具，操作剪贴板
+
+      fastfetch
+      btop
+    ]
+  );
+}
