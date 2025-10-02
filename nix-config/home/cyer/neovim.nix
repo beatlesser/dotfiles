1fd53ff9d323fc8e8b09ec435e24f61719c0c1e2
cyer <cyer@nixos.localdomain>
2025-10-03 04:08:43 +0800

    update config

--- nix-config/home/cyer/neovim.nix
+++ nix-config/home/cyer/neovim.nix
@@ -3,7 +3,7 @@
   programs.neovim.enable = true;
 
   xdg.configFile."nvim" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nvim";
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/.config/nvim";
   };
 
 }
