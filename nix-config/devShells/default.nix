c245c0e707360de310da5f018265e02077a1c3dc
cyer <cyer@wsl.localdomain>
2025-10-02 21:27:25 +0800

    update configs

--- nix-config/devShells/default.nix
+++ nix-config/devShells/default.nix
@@ -1,6 +1,9 @@
-{ lib,stable,unstable, ... }:
-
-let
+{
+  lib,
+  stable,
+  unstable,
+  ...
+}: let
   modules = {
     default = [
       "node"
@@ -14,49 +17,47 @@ let
       "node"
       "python"
     ];
-    node = [ "node" ];
-    python = [ "python" ];
-    rust = [ "rust" ];
-    lua = [ "lua" ];
-    go = [ "go" ];
-    c = [ "c" ];
+    node = ["node"];
+    python = ["python"];
+    rust = ["rust"];
+    lua = ["lua"];
+    go = ["go"];
+    c = ["c"];
+  };
+  extrasPkgs = ["fish"];
+
+  loadModules = moduleList: let
+    imported = map (name: import ./${name}.nix {inherit stable unstable;}) moduleList;
+  in {
+    packages = lib.flatten (map (m: m.packages or []) imported) ++ extrasPkgs;
+    nativeBuildInputs = lib.flatten (map (m: m.nativeBuildInputs or []) imported);
+    propagatedBuildInputs = lib.flatten (map (m: m.propagatedBuildInputs or []) imported);
+    propagatedNativeBuildInputs = lib.flatten (
+      map (m: m.propagatedNativeBuildInputs or []) imported
+    );
+    inputsFrom = lib.flatten (map (m: m.inputsFrom or []) imported);
+    env = builtins.foldl' (acc: m: acc // (m.env or {})) {} imported;
+    shellHook = builtins.concatStringsSep "\n" (map (m: m.shellHook or "") imported);
   };
 
-  loadModules =
-    moduleList:
-    let
-      imported = map (name: import ./${name}.nix { inherit stable unstable; }) moduleList;
-    in
-    {
-      packages = lib.flatten (map (m: m.packages or [ ]) imported);
-      nativeBuildInputs = lib.flatten (map (m: m.nativeBuildInputs or [ ]) imported);
-      propagatedBuildInputs = lib.flatten (map (m: m.propagatedBuildInputs or [ ]) imported);
-      propagatedNativeBuildInputs = lib.flatten (
-        map (m: m.propagatedNativeBuildInputs or [ ]) imported
-      );
-      inputsFrom = lib.flatten (map (m: m.inputsFrom or [ ]) imported);
-      env = builtins.foldl' (acc: m: acc // (m.env or { })) { } imported;
-      shellHook = builtins.concatStringsSep "\n" (map (m: m.shellHook or "") imported);
-    };
-
   shells = builtins.mapAttrs (_name: mods: loadModules mods) modules;
-
 in
-builtins.mapAttrs (
-  name: s:
-  stable.mkShell {
-    buildInputs = s.packages;
-    nativeBuildInputs = s.nativeBuildInputs;
-    propagatedBuildInputs = s.propagatedBuildInputs;
-    propagatedNativeBuildInputs = s.propagatedNativeBuildInputs;
-    inputsFrom = s.inputsFrom;
+  builtins.mapAttrs (
+    name: s:
+      stable.mkShell {
+        buildInputs = s.packages;
+        nativeBuildInputs = s.nativeBuildInputs;
+        propagatedBuildInputs = s.propagatedBuildInputs;
+        propagatedNativeBuildInputs = s.propagatedNativeBuildInputs;
+        inputsFrom = s.inputsFrom;
 
-    shellHook = ''
-      ${s.shellHook}
-      ${builtins.concatStringsSep "\n" (
-        map (k: "export ${k}=\"${s.env.${k}}\"") (builtins.attrNames s.env)
-      )}
-      echo "✅ ${name} DevShell ready"
-    '';
-  }
-) shells
+        shellHook = ''
+          ${s.shellHook}
+          ${builtins.concatStringsSep "\n" (
+            map (k: "export ${k}=\"${s.env.${k}}\"") (builtins.attrNames s.env)
+          )}
+          echo "✅ ${name} DevShell ready"
+        '';
+      }
+  )
+  shells
