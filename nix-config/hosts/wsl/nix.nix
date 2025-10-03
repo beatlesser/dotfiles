1d37c728818f7f04c75297ea54e9a12d1715b40d
cyer <cyer@wsl.localdomain>
2025-10-04 00:41:49 +0800

    update

--- nix-config/hosts/wsl/nix.nix
+++ nix-config/hosts/wsl/nix.nix
@@ -1,5 +1,6 @@
 {
-    pkgs,
+  pkgs,
+  inputs,
   ...
 }: let
   inherit (import ./vars.nix) username;
@@ -11,6 +12,7 @@ in {
   nix = {
     package = pkgs.lix;
     channel.enable = true;
+    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
     extraOptions = ''
       warn-dirty = false
     '';
@@ -26,11 +28,9 @@ in {
       ];
       substituters = [
         "https://nix-community.cachix.org"
-        "https://cache.nixos.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
       trusted-users = [
         "root"
