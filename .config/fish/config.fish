52cb77610d5d83b8f88ef7c0e7d4cdb5f6600c6f
beatlesser <2429209973@qq.com>
2025-10-16 04:26:22 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,7 +23,7 @@ if status is-interactive # Commands to run in interactive sessions can go here
     #alias
     alias vi 'nvim'
     alias ls 'lsd'
-    alias la 'ls -a'
-    alias ll 'ls -la'
+    alias la 'ls -ha'
+    alias ll 'ls -hl'
 end
 
