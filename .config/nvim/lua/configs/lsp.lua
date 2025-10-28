b25730d16ea1c60d8cbfe431992389558b9459ad
beatlesser <2429209973@qq.com>
2025-10-28 21:55:48 +0800

    [Nvim] update nvim lsp config

--- .config/nvim/lua/configs/lsp.lua
+++ .config/nvim/lua/configs/lsp.lua
@@ -1,152 +1,134 @@
 local LspConfig = {}
 
 function LspConfig.apply()
-    local lsp_list = {
-        'lua_ls',
-        'biome',
-        'ts_ls',
-        'gopls'
-    }
+	local lsp_list = {
+		"lua_ls",
+		"biome",
+		"ts_ls",
+		"gopls",
+	}
 
-    for _, name in ipairs(lsp_list) do
-        vim.lsp.enable(name)
-    end
+	for _, name in ipairs(lsp_list) do
+		vim.lsp.enable(name)
+	end
 
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
-      callback = function(event)
-        -- notification wrapper of original gd
-        vim.keymap.set('n', 'gd', function()
-          local params = vim.lsp.util.make_position_params(0, 'utf-8')
-          vim.lsp.buf_request(0, 'textDocument/definition', params, function(_, result, _, _)
-            if not result or vim.tbl_isempty(result) then
-              vim.notify('No definition found', vim.log.levels.INFO)
-            else
-              require('snacks').picker.lsp_definitions()
-            end
-          end)
-        end, { buffer = event.buf, desc = 'LSP: Goto Definition' })
+	vim.api.nvim_create_autocmd("LspAttach", {
+		group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+		callback = function(event)
+			-- toggle diagnostics
+			vim.keymap.set(
+				"n",
+				"<leader>td",
+				(function()
+					local diag_status = 1 -- 1 is show; 0 is hide
+					return function()
+						if diag_status == 1 then
+							diag_status = 0
+							vim.diagnostic.config({
+								underline = false,
+								virtual_text = false,
+								signs = false,
+								update_in_insert = false,
+							})
+						else
+							diag_status = 1
+							vim.diagnostic.config({
+								underline = true,
+								virtual_text = true,
+								signs = true,
+								update_in_insert = true,
+							})
+						end
+					end
+				end)(),
+				{ buffer = event.buf, desc = "LSP: Toggle diagnostics display" }
+			)
 
-        -- open gd in new split
-        vim.keymap.set('n', 'gD', function()
-          local win = vim.api.nvim_get_current_win()
-          local width = vim.api.nvim_win_get_width(win)
-          local height = vim.api.nvim_win_get_height(win)
+			-- folding
+			local client = vim.lsp.get_client_by_id(event.data.client_id)
+			if client and client.supports_method("textDocument/foldingRange") then
+				local win = vim.api.nvim_get_current_win()
+				vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+			end
 
-          -- Mimic tmux formula: 8 * width - 20 * height
-          local value = 8 * width - 20 * height
-          if value < 0 then
-            vim.cmd 'split' -- vertical space is more: horizontal split
-          else
-            vim.cmd 'vsplit' -- horizontal space is more: vertical split
-          end
+			-- Inlay hint
+			if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+				-- vim.lsp.inlay_hint.enable()
+				vim.keymap.set("n", "<leader>th", function()
+					vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+				end, { buffer = event.buf, desc = "LSP: Toggle Inlay Hints" })
+			end
 
-          vim.lsp.buf.definition()
-        end, { buffer = event.buf, desc = 'LSP: Goto Definition (split)' })
+			-- Highlight words under cursor
+			if
+				client
+				and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight)
+				and vim.bo.filetype ~= "bigfile"
+			then
+				local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+					buffer = event.buf,
+					group = highlight_augroup,
+					callback = vim.lsp.buf.document_highlight,
+				})
 
-        -- toggle diagnostics
-        vim.keymap.set(
-          'n',
-          '<leader>td',
-          (function()
-            local diag_status = 1 -- 1 is show; 0 is hide
-            return function()
-              if diag_status == 1 then
-                diag_status = 0
-                vim.diagnostic.config { underline = false, virtual_text = false, signs = false, update_in_insert = false }
-              else
-                diag_status = 1
-                vim.diagnostic.config { underline = true, virtual_text = true, signs = true, update_in_insert = true }
-              end
-            end
-          end)(),
-          { buffer = event.buf, desc = 'LSP: Toggle diagnostics display' }
-        )
+				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+					buffer = event.buf,
+					group = highlight_augroup,
+					callback = vim.lsp.buf.clear_references,
+				})
 
-        -- folding
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.supports_method 'textDocument/foldingRange' then
-          local win = vim.api.nvim_get_current_win()
-          vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
-        end
+				vim.api.nvim_create_autocmd("LspDetach", {
+					group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+					callback = function(event2)
+						vim.lsp.buf.clear_references()
+						vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
+						-- vim.cmd 'setl foldexpr <'
+					end,
+				})
+			end
+		end,
+	})
 
-        -- Inlay hint
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-          -- vim.lsp.inlay_hint.enable()
-          vim.keymap.set('n', '<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-          end, { buffer = event.buf, desc = 'LSP: Toggle Inlay Hints' })
-        end
+	local api, lsp = vim.api, vim.lsp
+	api.nvim_create_user_command("LspInfo", ":checkhealth vim.lsp", { desc = "Alias to `:checkhealth vim.lsp`" })
+	api.nvim_create_user_command("LspLog", function()
+		vim.cmd(string.format("tabnew %s", lsp.get_log_path()))
+	end, {
+		desc = "Opens the Nvim LSP client log.",
+	})
+	local complete_client = function(arg)
+		return vim.iter(vim.lsp.get_clients())
+			:map(function(client)
+				return client.name
+			end)
+			:filter(function(name)
+				return name:sub(1, #arg) == arg
+			end)
+			:totable()
+	end
+	api.nvim_create_user_command("LspRestart", function(info)
+		for _, name in ipairs(info.fargs) do
+			if vim.lsp.config[name] == nil then
+				vim.notify(("Invalid server name '%s'"):format(info.args))
+			else
+				vim.lsp.enable(name, false)
+			end
+		end
 
-        -- Highlight words under cursor
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) and vim.bo.filetype ~= 'bigfile' then
-          local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.document_highlight,
-          })
-
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.clear_references,
-          })
-
-          vim.api.nvim_create_autocmd('LspDetach', {
-            group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
-            callback = function(event2)
-              vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
-              -- vim.cmd 'setl foldexpr <'
-            end,
-          })
-        end
-      end,
-    })
-
-
-    local api, lsp = vim.api, vim.lsp
-    api.nvim_create_user_command('LspInfo', ':checkhealth vim.lsp', { desc = 'Alias to `:checkhealth vim.lsp`' })
-    api.nvim_create_user_command('LspLog', function()
-      vim.cmd(string.format('tabnew %s', lsp.get_log_path()))
-    end, {
-      desc = 'Opens the Nvim LSP client log.',
-    })
-    local complete_client = function(arg)
-      return vim
-        .iter(vim.lsp.get_clients())
-        :map(function(client)
-          return client.name
-        end)
-        :filter(function(name)
-          return name:sub(1, #arg) == arg
-        end)
-        :totable()
-    end
-    api.nvim_create_user_command('LspRestart', function(info)
-      for _, name in ipairs(info.fargs) do
-        if vim.lsp.config[name] == nil then
-          vim.notify(("Invalid server name '%s'"):format(info.args))
-        else
-          vim.lsp.enable(name, false)
-        end
-      end
-
-      local timer = assert(vim.uv.new_timer())
-      timer:start(500, 0, function()
-        for _, name in ipairs(info.fargs) do
-          vim.schedule_wrap(function(x)
-            vim.lsp.enable(x)
-          end)(name)
-        end
-      end)
-    end, {
-      desc = 'Restart the given client(s)',
-      nargs = '+',
-      complete = complete_client,
-    })
-
+		local timer = assert(vim.uv.new_timer())
+		timer:start(500, 0, function()
+			for _, name in ipairs(info.fargs) do
+				vim.schedule_wrap(function(x)
+					vim.lsp.enable(x)
+				end)(name)
+			end
+		end)
+	end, {
+		desc = "Restart the given client(s)",
+		nargs = "+",
+		complete = complete_client,
+	})
 end
 
 return LspConfig
