6a62b17fd24d9ad83866d396989ce0a4df6a0a6a
beatlesser <2429209973@qq.com>
2025-10-28 23:40:12 +0800

    [Nvim]: lsp update

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -3,28 +3,31 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 local CodeFormatter = {
-    'stevearc/conform.nvim',
-    event = { 'BufReadPost', 'BufNewFile' },
-    opts = {},
+	"stevearc/conform.nvim",
+	event = { "BufReadPost", "BufNewFile" },
+	opts = {},
 
-    config = function()
-        require('conform').setup {
-            formatters_by_ft = {
-                lua = { 'stylua' },
-                nix = {'alejandra','nixfmt','nixpkgs-fmt'},
-                xml = {'xmlformatter'}, 
-                kdl = {'kdlfmt'},
-            },
-        }
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua", "ast-grep" },
+				go = { "goimports", "ast-grep" },
+				typescript = { "biome", "ast-grep" },
+				json = { "biome" },
+				nix = { "alejandra", "nixfmt", "nixpkgs-fmt" },
+				xml = { "xmlformatter" },
+				kdl = { "kdlfmt" },
+			},
+		})
 
-        local do_format = function()
-            require('conform').format { async = true, lsp_fallback = true }
-        end
+		local do_format = function()
+			require("conform").format({ async = true, lsp_fallback = true })
+		end
 
-        vim.keymap.set('n', '<leader>lf', do_format, { desc = 'Format Current Buffer', noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>lf", do_format, { desc = "Format Current Buffer", noremap = true, silent = true })
 
-        vim.api.nvim_create_user_command('Format', do_format, { desc = 'Format Current Buffer' })
-    end,
+		vim.api.nvim_create_user_command("Format", do_format, { desc = "Format Current Buffer" })
+	end,
 }
 
 return CodeFormatter
