614ad24040d5c64e566d8656a8d56c9ce4e208f6
beatlesser <beatlesser@2925.com>
2025-12-31 17:34:29 +0800

    nix: update secret config

--- nix-config/secrets/default.nix
+++ nix-config/secrets/default.nix
@@ -3,14 +3,13 @@
   config,
   exvars,
   ...
-}:
-{
+}: {
   environment.systemPackages = with pkgs; [
     sops
     age
   ];
-  sops.defaultSopsFile = ../secrets.yaml;
-  sops.age.sshKeyPaths = [ "/home/${exvars.username}/.ssh/id_ed25519" ];
+  sops.defaultSopsFile = ../../secrets.yaml;
+  sops.age.sshKeyPaths = ["/home/${exvars.username}/.ssh/id_ed25519"];
   sops.age.keyFile = "/home/${exvars.username}/.config/sops/age/keys.txt";
   sops.age.generateKey = true;
   sops.secrets."subscriptions/owo" = {
